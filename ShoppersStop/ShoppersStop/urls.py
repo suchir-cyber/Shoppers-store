1c8329e4374cd1de8bef39107f28e4958a87b31e
suchir-cyber <suchirpandula@gmail.com>
2024-08-10 22:28:23 +0530

    added responsiveness

--- ShoppersStop/ShoppersStop/urls.py
+++ ShoppersStop/ShoppersStop/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
-from head.views import index
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/',views.BASE, name = 'base'),
     path('',views.HOME,name = 'home'),
-]
+] 
+
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
