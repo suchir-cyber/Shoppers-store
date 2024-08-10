e49693dc2a3a80168af0fa0463c7b78601422482
suchir-cyber <suchirpandula@gmail.com>
2024-08-10 11:13:50 +0530

    updated assets

--- ShoppersStop/ShoppersStop/settings.py
+++ ShoppersStop/ShoppersStop/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'ShoppersStop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,13 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = '/static'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles')]
-
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
