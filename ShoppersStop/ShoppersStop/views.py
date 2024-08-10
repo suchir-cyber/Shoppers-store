d2c367f4ce19bfd477c0b075890e699e09db71d9
suchir-cyber <suchirpandula@gmail.com>
2024-08-10 19:32:34 +0530

    added data products

--- ShoppersStop/ShoppersStop/views.py
+++ ShoppersStop/ShoppersStop/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render,redirect
+from head.models import Product
 
 def BASE(request):
     return render(request,'Main/base.html')
 
 def HOME(request):
-    return render(request,'Main/index.html')
+    products = Product.objects.all()
+    context = {
+        'products' : products
+    }
+    return render(request,'Main/index.html',context)
