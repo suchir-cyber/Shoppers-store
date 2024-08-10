e44d92609a386798250acbc774a95434f7b759a2
suchir-cyber <suchirpandula@gmail.com>
2024-08-10 17:28:59 +0530

    completed models

--- ShoppersStop/head/admin.py
+++ ShoppersStop/head/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import *
 
+class ImageTublerinline(admin.TabularInline):
+    model = Images
+
+
+class TagTublerinline(admin.TabularInline):
+    model = Tag
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ImageTublerinline,TagTublerinline]
+
+
 # Register your models here.
 admin.site.register(Categories)
 admin.site.register(Brand)
 admin.site.register(Color)
 admin.site.register(Filter_Price)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Images)
+admin.site.register(Tag)
