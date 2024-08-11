7862210dcca88574ea5893963391d8e194ca4825
suchir-cyber <suchirpandula@gmail.com>
2024-08-11 23:53:37 +0530

    updated single product page

--- ShoppersStop/ShoppersStop/settings.py
+++ ShoppersStop/ShoppersStop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # custom apps
     'head',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
--- ShoppersStop/head/models.py
+++ ShoppersStop/head/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 class Categories(models.Model):
     name = models.CharField(max_length = 200)
@@ -44,8 +44,8 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField()
     condition = models.CharField(choices=CONDITION, max_length=100)
-    information = models.TextField()
-    description = models.TextField()
+    information = RichTextField(null=True)
+    description = RichTextField(null=True)
     stock = models.CharField(choices=STOCK, max_length=200)
     status = models.CharField(choices=STATUS, max_length=200)
     created_date = models.DateTimeField(default=timezone.now)
